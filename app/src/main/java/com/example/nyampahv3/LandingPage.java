a3ac14aceac865e270efade7ed0dc46495e431a1
RakaZuhdi <rakazhp@gmail.com>
2022-11-29 10:22:45 +0700

    First Commit

---
+++ app/src/main/java/com/example/nyampahv3/LandingPage.java
@@ -0,0 +1,14 @@
+package com.example.nyampahv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
+}
---
+++ app/src/main/java/com/example/nyampahv3/LoginAsPage.java
@@ -0,0 +1,14 @@
+package com.example.nyampahv3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginAsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_as_page);
+    }
+}
