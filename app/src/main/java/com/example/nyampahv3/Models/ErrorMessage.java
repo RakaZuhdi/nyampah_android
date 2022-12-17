533e04587e87a0722d5633663641566015db2463
git-denial <danielamazia@gmail.com>
2022-12-17 12:26:08 +0700

    Add ErrorMessage class

---
+++ app/src/main/java/com/example/nyampahv3/Models/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.example.nyampahv3.Models;
+
+public class ErrorMessage {
+    public String http_code;
+    public String code;
+    public String timestamp;
+    public String error_message;
+}
