810d52e4777fbe6046bcb2e0c2af14d75285529a
git-denial <danielamazia@gmail.com>
2022-12-17 13:06:18 +0700

    Add ApiUtil

--- app/src/main/java/com/example/nyampahv3/Utils/ApiUtil.java
+++ app/src/main/java/com/example/nyampahv3/Utils/ApiUtil.java
@@ -1,29 +1,11 @@
-package com.example.nyampahv3.Utils;/*
- * Copyright (C) 2014 Square, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.example.nyampahv3.Utils;
 
-import android.os.Build;
-import android.util.Log;
-
-import androidx.annotation.RequiresApi;
 
 import com.example.nyampahv3.Models.ErrorMessage;
 import com.example.nyampahv3.Models.User;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.squareup.okhttp.HttpUrl;
+
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -35,8 +17,11 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public final class ApiUtil {
@@ -44,7 +29,7 @@ public final class ApiUtil {
     private static Gson gson = new Gson();
     private static String base_url = "http://10.0.2.2:3000";
 
-    public static <T> T get(String requestUrl, Map<String ,String> param, Type T) throws Exception {
+    public static <T> T GET(String requestUrl, Map<String ,String> param, Type T) throws Exception {
         final OkHttpClient client = new OkHttpClient();
 
         String url = base_url + '/' + requestUrl;
@@ -79,6 +64,159 @@ public final class ApiUtil {
 
 
         if(json instanceof JSONObject && ((JSONObject) json).getString("http_code").equals("403")){
+            ErrorMessage err = gson.fromJson(json.toString(), ErrorMessage.class);
+            System.err.println(err.code);
+            System.err.println(err.error_message);
+            return null;
+
+        }
+
+
+        return  gson.fromJson( json.toString(), T);
+    }
+
+    public static <T> T POST(String requestUrl, Map<String ,String> param, String body ,Type T) throws Exception {
+        final OkHttpClient client = new OkHttpClient();
+
+        String url = base_url + '/' + requestUrl;
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        //val body = jsonObject.toString().toRequestBody(mediaType)
+        RequestBody reqbody = RequestBody.create(body, mediaType);
+
+        System.out.println(url.toString());
+        HttpUrl.Builder queryUrlBuilder = HttpUrl.parse(url).newBuilder();
+
+
+        if(param != null)
+            for(Map.Entry<String, String> e : param.entrySet())
+            {
+                queryUrlBuilder.addQueryParameter(e.getKey(), e.getValue());
+            }
+
+        Request request = new Request.Builder()
+                .url(String.valueOf(queryUrlBuilder.build()))
+                .post(reqbody)
+                .build();
+
+
+        CompletableFuture<Response> future = new CompletableFuture<>();
+
+        client.newCall(request).enqueue(toCallback(future));
+        Response response = future.get();
+
+        String strespon = response.body().string();
+
+        T json = strespon.startsWith("[")
+                ? (T) new JSONArray(strespon)
+                : (T) new JSONObject(strespon);
+
+
+
+        if(json instanceof JSONObject &&
+                ((JSONObject) json).has("http_code") &&
+                ((JSONObject) json).getString("http_code").equals("403")){
+            ErrorMessage err = gson.fromJson(json.toString(), ErrorMessage.class);
+            System.err.println(err.code);
+            System.err.println(err.error_message);
+            return null;
+
+        }
+
+
+        return  gson.fromJson( json.toString(), T);
+    }
+
+    public static <T> T PUT(String requestUrl, Map<String ,String> param, String body ,Type T) throws Exception {
+        final OkHttpClient client = new OkHttpClient();
+
+        String url = base_url + '/' + requestUrl;
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        //val body = jsonObject.toString().toRequestBody(mediaType)
+        RequestBody reqbody = RequestBody.create(body, mediaType);
+
+        System.out.println(url.toString());
+        HttpUrl.Builder queryUrlBuilder = HttpUrl.parse(url).newBuilder();
+
+
+        if(param != null)
+            for(Map.Entry<String, String> e : param.entrySet())
+            {
+                queryUrlBuilder.addQueryParameter(e.getKey(), e.getValue());
+            }
+
+        Request request = new Request.Builder()
+                .url(String.valueOf(queryUrlBuilder.build()))
+                .put(reqbody)
+                .build();
+
+
+        CompletableFuture<Response> future = new CompletableFuture<>();
+
+        client.newCall(request).enqueue(toCallback(future));
+        Response response = future.get();
+
+        String strespon = response.body().string();
+
+        T json = strespon.startsWith("[")
+                ? (T) new JSONArray(strespon)
+                : (T) new JSONObject(strespon);
+
+
+
+        if(json instanceof JSONObject &&
+                ((JSONObject) json).has("http_code") &&
+                ((JSONObject) json).getString("http_code").equals("403")){
+            ErrorMessage err = gson.fromJson(json.toString(), ErrorMessage.class);
+            System.err.println(err.code);
+            System.err.println(err.error_message);
+            return null;
+
+        }
+
+
+        return  gson.fromJson( json.toString(), T);
+    }
+
+    public static <T> T DELETE(String requestUrl, Map<String ,String> param, String body ,Type T) throws Exception {
+        final OkHttpClient client = new OkHttpClient();
+
+        String url = base_url + '/' + requestUrl;
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        //val body = jsonObject.toString().toRequestBody(mediaType)
+        RequestBody reqbody = RequestBody.create(body, mediaType);
+
+        System.out.println(url.toString());
+        HttpUrl.Builder queryUrlBuilder = HttpUrl.parse(url).newBuilder();
+
+
+        if(param != null)
+            for(Map.Entry<String, String> e : param.entrySet())
+            {
+                queryUrlBuilder.addQueryParameter(e.getKey(), e.getValue());
+            }
+
+        Request request = new Request.Builder()
+                .url(String.valueOf(queryUrlBuilder.build()))
+                .delete(reqbody)
+                .build();
+
+
+        CompletableFuture<Response> future = new CompletableFuture<>();
+
+        client.newCall(request).enqueue(toCallback(future));
+        Response response = future.get();
+
+        String strespon = response.body().string();
+
+        T json = strespon.startsWith("[")
+                ? (T) new JSONArray(strespon)
+                : (T) new JSONObject(strespon);
+
+
+
+        if(json instanceof JSONObject &&
+                ((JSONObject) json).has("http_code") &&
+                ((JSONObject) json).getString("http_code").equals("403")){
             ErrorMessage err = gson.fromJson(json.toString(), ErrorMessage.class);
             System.err.println(err.code);
             System.err.println(err.error_message);
