5d855e8a30c44a60c15b9d70b59ad4e248cb565d
git-denial <danielamazia@gmail.com>
2022-12-19 19:16:37 +0700

    Add writeSharedPreferenceString function

--- app/src/main/java/com/example/nyampahv3/Utils/SystemUtil.java
+++ app/src/main/java/com/example/nyampahv3/Utils/SystemUtil.java
@@ -1,5 +1,10 @@
 package com.example.nyampahv3.Utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.nyampahv3.R;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -12,5 +17,11 @@ public class SystemUtil {
             // Log error
         }
         return false;
+    }
+
+    public static void writeSharedPreferenceString(SharedPreferences sharedPref, String name, String data){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(name, data);
+        editor.apply();
     }
 }
