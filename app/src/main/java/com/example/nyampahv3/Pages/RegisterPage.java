d6a42b4b1396b034014a7155f9cd0efee577a178
git-denial <danielamazia@gmail.com>
2022-12-24 15:17:22 +0700

    Add register user functionality

--- app/src/main/java/com/example/nyampahv3/Pages/RegisterPage.java
+++ app/src/main/java/com/example/nyampahv3/Pages/RegisterPage.java
@@ -1,16 +1,82 @@
 package com.example.nyampahv3.Pages;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nyampahv3.Apis.UserApi;
+import com.example.nyampahv3.Models.ErrorMessage;
+import com.example.nyampahv3.Models.User;
 import com.example.nyampahv3.R;
+import com.example.nyampahv3.Utils.SystemUtil;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 public class RegisterPage extends AppCompatActivity {
 
+    EditText fullname;
+    EditText email;
+    EditText password;
+    EditText confirm_password;
+    Button registerBtn;
+    Gson gson = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+
+        fullname = findViewById(R.id.textInput_fullname);
+        email = findViewById(R.id.textInput_email);
+        password = findViewById(R.id.textInput_password);
+        confirm_password = findViewById(R.id.textInput_confirm_password);
+        registerBtn = findViewById(R.id.button_submit);
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+
+                    String fullname_ = fullname.getText().toString();
+                    String email_ = email.getText().toString();
+                    String password_ = password.getText().toString();
+                    String confirm_password_ = confirm_password.getText().toString();
+
+                    if(!password_.equals(confirm_password_)){
+                        Toast.makeText(getApplicationContext(), "Confirm your password !", Toast.LENGTH_LONG);
+                        return;
+                    }
+
+
+                    User newUser = new User(fullname_,email_,password_);
+
+                    User register = UserApi.Register(newUser);
+
+                    if(register.created_at == null){
+
+                        var ermsg = new ErrorMessage(new JSONObject(gson.toJson(register)));
+
+                        Toast.makeText(getApplicationContext(), ermsg.error_message, Toast.LENGTH_LONG);
+                        return;
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Register success", Toast.LENGTH_LONG);
+
+                        startActivity(new Intent(getApplicationContext(),LoginPage.class));
+                        return;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
