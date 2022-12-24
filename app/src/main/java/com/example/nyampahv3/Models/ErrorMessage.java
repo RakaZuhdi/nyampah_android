fa608f573c646b49450de4e52f942455b716aaca
git-denial <danielamazia@gmail.com>
2022-12-24 11:38:45 +0700

    Add constructor in ErrorMessage

--- app/src/main/java/com/example/nyampahv3/Models/ErrorMessage.java
+++ app/src/main/java/com/example/nyampahv3/Models/ErrorMessage.java
@@ -1,8 +1,18 @@
 package com.example.nyampahv3.Models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ErrorMessage {
     public String http_code;
     public String code;
     public String timestamp;
     public String error_message;
+
+    public ErrorMessage(JSONObject e) throws JSONException {
+        this.http_code = e.getString("http_code");
+        this.code = e.getString("code");
+        this.timestamp = e.getString("timestamp");
+        this.error_message = e.getString("error_message");
+    }
 }
