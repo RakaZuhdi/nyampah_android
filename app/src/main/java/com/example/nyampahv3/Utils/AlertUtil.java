1930b419592ef6006789f83dda4120dca5330b14
git-denial <danielamazia@gmail.com>
2022-12-26 14:08:45 +0700

    Add AlertUtil

---
+++ app/src/main/java/com/example/nyampahv3/Utils/AlertUtil.java
@@ -0,0 +1,59 @@
+package com.example.nyampahv3.Utils;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class AlertUtil {
+
+    public static void alert(String title, String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(App.getActivity());
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(true);
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    public static void alertWithYesNo(String title, String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(App.getActivity());
+
+        // set title
+        alertDialogBuilder.setTitle(title);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        App.getActivity().finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+}
