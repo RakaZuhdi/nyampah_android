738b26b0e05b19b53ab5fd065db8790713ae3084
git-denial <danielamazia@gmail.com>
2022-12-26 14:08:13 +0700

    Add App class

---
+++ app/src/main/java/com/example/nyampahv3/Utils/App.java
@@ -0,0 +1,23 @@
+package com.example.nyampahv3.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class App {
+    private static Context context;
+    private static Activity activity;
+
+    public static void setContext(Context cntxt) {
+        context = cntxt;
+    }
+    public static void setActivity(Activity ac) {
+        activity = ac;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+    public static Activity getActivity() {
+        return activity;
+    }
+}
