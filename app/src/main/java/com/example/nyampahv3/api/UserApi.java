b2c301ba07be1a7319684d071bf5a25cee1c62a9
git-denial <danielamazia@gmail.com>
2023-01-03 11:13:11 +0700

    Add UserApi

---
+++ app/src/main/java/com/example/nyampahv3/api/UserApi.java
@@ -0,0 +1,65 @@
+package com.example.nyampahv3.api;
+
+import com.example.nyampahv3.model.User;
+import com.example.nyampahv3.util.ApiUtil;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class UserApi {
+
+    static Gson gson = new Gson();
+    static Type type = User.class;
+    static Type lisType = User[].class;
+
+
+    public static User[] GetAll() throws Exception {
+
+        User[] users = {};
+
+        users =  ApiUtil.GET("v1/users", null, lisType);
+
+//        if(result == null) return new JSONArray(new ArrayList<String>());;
+        if(users == null) return new User[0];
+
+//        return result.toJSONArray(result.names());
+        return users;
+
+    }
+
+
+    public static User Login(String email, String password) throws Exception {
+
+        JSONObject credential  = new JSONObject();
+        credential.put("email",email);
+        credential.put("password",password);
+
+        User user =  ApiUtil.POST("v1/user/login", null, credential.toString(), type);
+
+        return user;
+    }
+
+    public static User TokenLogin() throws Exception {
+
+        JSONObject credential  = new JSONObject();
+
+        User user =  ApiUtil.POST("v1/user/token_login", null, credential.toString(), type);
+
+        return user;
+    }
+
+    public static User Register(User u) throws Exception {
+
+        User user =  ApiUtil.POST("v1/user", null, gson.toJson(u) , type);
+
+        return user;
+    }
+
+
+}
