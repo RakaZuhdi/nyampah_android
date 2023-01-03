02e08e004d58b5624eb31bbaa6e8c23e10099aef
RakaZuhdi <rakazhp@gmail.com>
2023-01-03 17:47:54 +0700

    App now logins automatically using token login

--- app/src/main/java/com/example/nyampahv3/LandingPage.java
+++ app/src/main/java/com/example/nyampahv3/LandingPage.java
@@ -3,16 +3,23 @@ package com.example.nyampahv3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.nyampahv3.api.UserApi;
+import com.example.nyampahv3.model.User;
 import com.example.nyampahv3.util.App;
 
 public class LandingPage extends AppCompatActivity {
 
     private Button button_loginas;
     private Button button_register;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,19 @@ public class LandingPage extends AppCompatActivity {
                 openLoginPage();
             }
         });
+
+        try {
+            User result = UserApi.TokenLogin();
+            Log.d("test", result.fullname);
+
+            if(result.fullname != null){
+                Intent intent = new Intent(this, MainPageUser.class);
+                startActivity(intent);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         button_register = (Button) findViewById(R.id.button_register_landingpage);
         button_register.setOnClickListener(new View.OnClickListener(){
