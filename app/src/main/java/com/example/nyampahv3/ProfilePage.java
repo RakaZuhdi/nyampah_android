42b8fa05f771e25438b9afe0160e2eeac79d659b
RakaZuhdi <rakazhp@gmail.com>
2023-01-04 09:35:12 +0700

    Made profile page dynamic (not finished) Still need to add total trash deposited

--- app/src/main/java/com/example/nyampahv3/ProfilePage.java
+++ app/src/main/java/com/example/nyampahv3/ProfilePage.java
@@ -7,22 +7,48 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.nyampahv3.model.User;
+import com.example.nyampahv3.util.SystemUtil;
 import com.google.android.material.navigation.NavigationView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class ProfilePage extends AppCompatActivity {
 
     private NavigationView navigationView;
     private Button logoutButton;
+    private TextView text_fullname;
+    private TextView text_email;
+    private TextView text_total_trash;
+    private TextView text_total_pickups;
+    private TextView text_member_since;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_profile_activity);
+        User user = SystemUtil.getCurrentLoggedInUserDataSharedPref();
+
+        text_fullname = (TextView) findViewById(R.id.text_fullname_profile);
+        text_fullname.setText(user.fullname);
+        text_email = (TextView) findViewById(R.id.text_email_profile);
+        text_email.setText(user.email);
+        text_total_trash = (TextView) findViewById(R.id.text_totaltrash_profile);
+        text_total_trash.setText(user.total_trash_weight + "KG");
+        text_total_pickups = (TextView) findViewById(R.id.text_totalpickups_profile);
+        text_total_trash.setText("");
+        text_member_since = (TextView) findViewById(R.id.text_membersince_profile);
+        text_member_since.setText(String.valueOf(user.created_at));
+        Date date = new Date(String.valueOf(user.created_at));
+        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        text_member_since.setText("Member since " + dateFormat.format(date));
 
         logoutButton = (Button) findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +57,6 @@ public class ProfilePage extends AppCompatActivity {
                 SharedPreferences preferences = getSharedPreferences("login", 0);
                 Log.d("Preferences", preferences.getString("user_data_json",""));
                 preferences.edit().remove("user_data_json").commit();
-
                 openHomePage();
             }
         });
